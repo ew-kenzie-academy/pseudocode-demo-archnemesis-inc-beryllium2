c99b4a62df271507504750a03d342198d4fc64a6
ASUSTe-UX434IQ <redhat@Stewart>
2021-06-25 12:39:55 -0600

    Outline is forming...

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 // To acquire all of the required data; to print stuff.
 
-// - get the day of the week
-
 // - get employee's name
-
-// - get booleans for... is Friday holiday? is Monday holiday?
 
 // - get hours worked
 //   TODO:   get hours worked from HR database
 
+// - get the day of the week
+
 // - case(weekday) -> day specific message
 
+// - get booleans for... is Friday holiday? is Monday holiday?
 // - if it is thursday and Friday is a holiday
 //   or today is friday and Monday is holiday
 //     then alert about long weekend
 
-// - if associate hours worked > 35 then warn abouit overtime
-
+// - if associate hours worked > 35 then warn about overtime
 // - Print all messages with document.write
-
 // - MIME it up with HTML and CSS
+
+
