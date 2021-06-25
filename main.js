7e3f07a8524a4991d279cf856856ed4b15af0621
ASUSTe-UX434IQ <redhat@Stewart>
2021-06-25 12:28:59 -0600

    Unordered pseudocode outline

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-// STEP 1:
+// To acquire all of the required data; to print stuff.
+
+// - get the day of the week
+
+// - get employee's name
+
+// - get booleans for... is Friday holiday? is Monday holiday?
+
+// - get hours worked
+//   TODO:   get hours worked from HR database
+
+// - case(weekday) -> day specific message
+
+// - if it is thursday and Friday is a holiday
+//   or today is friday and Monday is holiday
+//     then alert about long weekend
+
+// - if associate hours worked > 35 then warn abouit overtime
+
+// - Print all messages with document.write
+
+// - MIME it up with HTML and CSS
