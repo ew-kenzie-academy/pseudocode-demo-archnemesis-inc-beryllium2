20555f93df4c46ea6cf72a46346f20563013a084
ASUSTe-UX434IQ <redhat@Stewart>
2021-06-25 13:57:33 -0600

    It might actually work

--- main.js
+++ main.js
@@ -1,41 +1,56 @@
 // To acquire all of the required data; to print stuff.
 
 // - get employee's name; what is your quest?
-userName=prompt("What is your name?");
+  userName=prompt("What is your name?");
 
 // - get hours worked
 //   TODO: SELECT HOURSWORKED from HR-database where USERNAME=userName;
-hoursWorked=35;
+  hoursWorked=31;
 
 // - get the day of the week
-day=new Date().toLocaleString('en-us', {  weekday: 'long' });
+  day=new Date().toLocaleString('en-us', {  weekday: 'long' });
+
+// - get is this Friday or next Monday a holiday?
+//   TODO: implement this feature
+  thisFridayIsHoliday=Math.random()<.15;
+  nextMondayIsHoliday=Math.random()<.15;
+  if(thisFridayIsHoliday && nextMondayIsHoliday)
+      nextMondayIsHoliday=false;
 
 // - case(weekday) -> day specific message
-switch (day){
-  case    "Sunday":    message="Do not work today :P";
-    break;
-  case    "Monday":    message="Sounds like somebody's got a case of the Mondays";
-    break;
-  case   "Tuesday":    message="Ruby Tuesday isn't good anymore";
-    break;
-  case "Wednesday":    message="Median day";
-    break;
-  case  "Thursday":    message="Its almost Friday";
-    break;
-  case    "Friday":    message="Happy friday";
-    break;
-  case  "Saturday":    message="Its the last day of the week.";
-    break;
-}
+  switch (day){
+    case    "Sunday":    message="Do not work today :P";
+      break;
+    case    "Monday":    message="Sounds like somebody's got a case of the Mondays";
+      break;
+    case   "Tuesday":    message="Ruby Tuesday isn't good anymore";
+      break;
+    case "Wednesday":    message="Median day";
+      break;
+    case  "Thursday":    message="Its almost Friday";
+      break;
+    case    "Friday":    message="Happy Friday";
+      break;
+    case  "Saturday":    message="Its the last day of the week.";
+      break;
+  }
 
-// - if it is thursday and Friday is a holiday
-//   or today is friday and Monday is holiday
-//     then alert about long weekend
+// - if there is an impending holdiday, then alert about long weekend
+//     TODO: Actually determine whether this is the case.
+  if(   (day === "Thursday" && thisFridayIsHoliday)
+     || (day === "Friday"   && nextMondayIsHoliday)  )
+           secondMessage="There is an upcoming holiday. Its going to be a long weekend.";
 
 // - if associate hours worked > 35 then warn about overtime
-// - Print all messages with document.write
-// - MIME it up with HTML and CSS
+  if(hoursWorked>31)
+      alert("You have worked "
+            + hoursWorked
+            + " this week. Be cautious and Do not work overtime!");
 
+// - Print all messages with document.write
+  document.write(message + secondMessage );
+
+// - MIME it up with HTML and CSS
 
 /*
 * Research
@@ -46,5 +61,24 @@ switch (day){
   + [[https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch][mdn::switch]]
     *switch*
     I wonder if I can't use it like a function and return the value of assignment
+  + [[https://www.federalpay.org/holidays][federalpay.org/holidays]]
+    *List of Federal Holidays - FederalPay.org*
+    There it is when I want to get around to it.
+    #+begin_text
+    |-------------------------------------+----------+---------------------|
+    | New Year's Day                      | Friday   | January 1st, 2021   |
+    | Birthday of Martin Luther King, Jr. | Monday   | January 18th, 2021  |
+    | Washington's Birthday               | Monday   | February 15th, 2021 |
+    | Memorial Day                        | Monday   | May 31st, 2021      |
+    | Juneteenth Independence Day †       | Friday   | June 18th, 2021     |
+    | Independence Day † (9 days)         | Monday   | July 5th, 2021      |
+    | Labor Day                           | Monday   | September 6th, 2021 |
+    | Columbus Day                        | Monday   | October 11th, 2021  |
+    | Veterans Day                        | Thursday | November 11th, 2021 |
+    | Thanksgiving Day                    | Thursday | November 25th, 2021 |
+    | Christmas Day                       | Friday   | December 24th, 2021 |
+    |-------------------------------------+----------+---------------------|
+    #+end_text
+    
 */
 
