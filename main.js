c971e85deb545e3fc9c62d2002cd01bffef87298
ASUSTe-UX434IQ <redhat@Stewart>
2021-06-25 12:53:12 -0600

    How to tell whether this is a holiday week

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
 // To acquire all of the required data; to print stuff.
 
 // - get employee's name
+userName=prompt("What is your name?");
 
 // - get hours worked
 //   TODO:   get hours worked from HR database
+hoursWorked=35;
 
 // - get the day of the week
+day=new Date().toLocaleString('en-us', {  weekday: 'long' });
 
 // - case(weekday) -> day specific message
+switch (day){
+  case "Sunday":
+    break;
+  case "Monday":
+    break;
+  case "Tuesday":
+    break;
+  case "Wednesday":
+    break;
+  case "Thursday":
+    break;
+  case "Friday":
+    console.log("today is friday");
+    break;
+  case "Saturday":
+    break;
+}
 
 // - get booleans for... is Friday holiday? is Monday holiday?
 // - if it is thursday and Friday is a holiday
@@ -18,4 +38,12 @@
 // - Print all messages with document.write
 // - MIME it up with HTML and CSS
 
+
+/*
+* Research
+  + [[https://stackoverflow.com/questions/9677757/how-to-get-the-day-of-the-week-from-the-day-number-in-javascript][<Gabriel Kohen>@stackoverflow::9677757]]
+    *How to get the day of the week from the day number in JavaScript?*
+    Do I care why it works? Plug and chug :P
+    ~console.log(new Date().toLocaleString('en-us', {  weekday: 'long' }));~
+*/
 
